af8d12ff3e8e381e6fcff87ddc977fa174d98339
Олег Яныбайевич <davletoleg@yandex.ru>
2022-12-07 20:40:07 +0500

    ЗД 2

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,33 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Домашнее задание. Переменные. Часть 2!");
+        //Задача 1
+        System.out.println("задача 1");
+        int a = 358642585;
+        System.out.println("Значение переменной 'a' с типом int равно " +a );
+        byte b = -96;
+        System.out.println("Значение переменной 'b' с типом byte равно " +b );
+        short c = 31624;
+        System.out.println("Значение переменной 'c' с типом scort равно " +c );
+        float d = -3.00000004f;
+        System.out.println("Значение переменной 'd' с типом float равно " +d );
+        double e = 1.00000000000000017;
+        System.out.println("Значение переменной 'e' с типом double равно " +e );
+        //Задача 2
+        System.out.println("задача 2");
+        float d1 = 27.12f;
+        long f1 = 987678965549l;
+        double e1 = 2.786;
+        boolean g1 = false;
+        char h1 = 569;
+        short c1 = 27897;
+        byte b1 = 67;
+
+
+
+
+
+
+
     }
 }
